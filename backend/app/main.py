903bd207c3c710346e050a77463e89985762a83d
alfredoenzyme <alfredo.marino@enzyme.biz>
2024-11-08 18:14:54 +0100

    backend hello world

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+from api.emergency_router import router as emergency_router
+
+app = FastAPI(
+    title="CatGPT API",
+    debug=True,
+    version="1.0.0"
+)
+app.include_router(emergency_router)
