0d437db3ac6f18578db47340e1baf3d52c45df0b
Danivilanova <dani.vila.gonz@gmail.com>
2024-11-09 10:33:05 +0100

    add fetchMunicipalities and sendAlert functions, implement utility className merging function

---
+++ frontendnj/lib/queries.ts
@@ -0,0 +1,25 @@
+export const fetchMunicipalities = async () => {
+  const res = await fetch("http://localhost:8000/municipalities");
+  return res.json();
+};
+
+export const sendAlert = async (
+  municipeCode: string,
+  alertMessage: string,
+  severity: string,
+  targetAudience: string
+) => {
+  const res = await fetch("http://localhost:8000/municipalities", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      municipe_code: municipeCode,
+      alert_message: alertMessage,
+      severity: severity,
+      target_audience: targetAudience,
+    }),
+  });
+  return res.json();
+};
